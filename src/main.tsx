5ed2e25513c0a67d6a056d26e6a43c4f7c4cabec
rakitenkogermans <rakitenko.germans@gmail.com>
2025-01-28 19:47:52 +0200

    feat: add husky and fix all eslint errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root') as HTMLDivElement;
+
+if (!container) {
+  throw new Error('root container is not found. Cant mount react application!');
+}
+
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
